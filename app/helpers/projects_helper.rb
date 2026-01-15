afa7f75bdd1da29272a5c2b844f66bb4bc602815
alex <alexjhayes1@hotmail.com>
2026-01-15 17:37:06 +0000

    try fixing workflows

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,25 @@
+require 'uri'
+
 module ProjectsHelper
+  # Return a URL string only if it parses as http or https; otherwise nil.
+  def safe_url(url)
+    return nil if url.blank?
+    uri = URI.parse(url)
+    return uri.to_s if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    nil
+  end
+
+  # Helper to link to external URLs only when they look safe. Adds noopener noreferrer.
+  def external_link_to(url, text = nil, **opts)
+    text ||= url
+    safe = safe_url(url)
+    if safe
+      opts[:target] ||= "_blank"
+      opts[:rel] = [opts[:rel], "noopener noreferrer"].compact.join(' ')
+      link_to text, safe, **opts
+    else
+      text
+    end
+  end
 end
