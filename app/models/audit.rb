c7c2c0b1e709ffe35a9fade64e2fa89ef9d0881f
Acidicts <alexjhayes1@hotmail.com>
2026-01-15 18:40:56 +0000

    Annotate Audit model (schema info)

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: audits
+#
+#  id         :bigint           not null, primary key
+#  action     :string           not null
+#  created_at :datetime         not null
+#  details    :json             default: {}
+#  project_id :integer
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
 class Audit < ApplicationRecord
   belongs_to :user
   belongs_to :project, optional: true
