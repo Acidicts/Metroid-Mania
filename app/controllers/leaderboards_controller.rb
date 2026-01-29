19c715d93785a09dbfdc012be6efa9fb6c1f370d
Acidicts <alexjhayes1@hotmail.com>
2026-01-29 21:47:56 +0000

    fix: exclude deleted users from leaderboard query i wasnt liking that deleted users were changing placements for learderboard

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -3,7 +3,7 @@ class LeaderboardsController < ApplicationController
     # Get all users with their total credits from ships
     # Use left outer join to include users with no ships
     # Sum credits_awarded from all ships for each user (NULL values are treated as 0)
-    @users = User.left_joins(:ships)
+    @users = User.where.not(name: "Deleted User").left_joins(:ships)
                  .select('users.*, COALESCE(SUM(ships.credits_awarded), 0) as total_credits')
                  .group('users.id')
                  .order('total_credits DESC')
